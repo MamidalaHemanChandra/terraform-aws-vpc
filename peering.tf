46e3a3a70f90c875786b3f4006f525672b9b3ed1
heman chandra <hemanchandra701@gmail.com>
2026-03-03 19:36:16 +0530

    terraform

--- peering.tf
+++ peering.tf
@@ -23,13 +23,15 @@ resource "aws_vpc_peering_connection" "roboshop" {
 }
 
 resource "aws_route" "public" {
+    count = var.peering_required ? 1 : 0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.roboshop.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.roboshop[0].id
 }
 
 resource "aws_route" "default" {
+    count = var.peering_required ? 1 : 0
   route_table_id            = data.aws_route_table.default.id
   destination_cidr_block    = var.vpc_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.roboshop.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.roboshop[0].id
 }
